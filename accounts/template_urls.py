d0b30018ce6e4bd038f67f67db803dd624d2af3f
Paul Onteri <onteripaul@gmail.com>
2020-10-23 09:37:53 +0300

    fix urls

--- accounts/template_urls.py
+++ accounts/template_urls.py
@@ -5,7 +5,7 @@ from accounts.views import signup, home
 
 urlpatterns = [
     path('', include('django.contrib.auth.urls')),
-    url('home', home, name='home'),
+    path('home', home, name='home'),
     url(r'^signup/$', signup, name='signup'),
-    url('', home, name='index'),
+    path('', home, name='index'),
 ]
